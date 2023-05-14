938ff6d5ba1a5cc8d8f5de7cf9d401ce4d66a675
Gurveer Madurai <maduraigurveer@gmail.com>
2023-05-13 20:00:35 -0700

    questions added to inquirer . prompt

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # svg-logo-maker
+
+# install
+npm init
+inquirer version 8.2.4
+jest version 24.9.0
--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-const inquirer = require('inquirer');
-const jest = require('jest');
+const inquirer = require("inquirer");
+
+inquirer
+    .prompt([
+    {
+        name: "shape",
+        message: "Choose the shape of your logo:",
+        type: "list",
+        choices: ['circle', 'triangle', 'square']
+    },
+    {
+        name: "test",
+        message: "Enter the three lettered text for your logo",
+        type: "input",
+    },
+    {
+        name: "color",
+        message: "Enter the color of your logo's text",
+        type: "input",
+    },
+    {
+        name: "bgColor",
+        message: "Enter the background color for your logo:",
+        type: "input",
+    },
+])
+    .then((responses)=>{
+return(responses);
+});
