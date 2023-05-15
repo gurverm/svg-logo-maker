711e0f94e90b0f1050c0a659faf79f3d3d4036db
Gurveer Madurai <maduraigurveer@gmail.com>
2023-05-15 10:17:00 -0700

    debug

--- library/shapes.test.js
+++ library/shapes.test.js
@@ -1,34 +1,31 @@
 const { Circle, Triangle, Square } = require("./shapes.js");
 
-// Unit testing -> testing for a circle with a #ca00ca background to render
 describe("Circle test", () => {
-    test("test for a circle with a #ca00ca background", () => {
-      const shape = new Circle();
-      shape.setColor("#ca00ca");
-      expect(shape.render()).toEqual(
-        '<circle cx="150" cy="115" r="80" fill="#ca00ca" />'
-      );
-    });
+  test("test for a circle with a #fff background", () => {
+    const shape = new Circle();
+    shape.setColor("#fff");
+    expect(shape.render()).toEqual(
+      '<circle cx="150" cy="115" r="80" fill="#fff" />'
+    );
   });
-// Unit testing -> testing for a triangle with a blue background to render
+});
+
 describe("Triangle test", () => {
-  test("test for a triangle with a blue background", () => {
+  test("test for a triangle with a red background", () => {
     const shape = new Triangle();
-    shape.setColor("blue");
+    shape.setColor("red");
     expect(shape.render()).toEqual(
-      '<polygon points="150, 18 244, 182 56, 182" fill="blue" />'
+      '<polygon points="150, 18 244, 182 56, 182" fill="red" />'
     );
   });
 });
 
-// Unit testing -> testing for a square with a purple background to render
 describe("Square test", () => {
-  test("test for a square with a purple background", () => {
+  test("test for a square with a green background", () => {
     const shape = new Square();
-    shape.setColor("purple");
+    shape.setColor("green");
     expect(shape.render()).toEqual(
-      '<rect x="73" y="40" width="160" height="160" fill="purple" />'
+      '<rect x="73" y="40" width="160" height="160" fill="green" />'
     );
   });
 });
-
